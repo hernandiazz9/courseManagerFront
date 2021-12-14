9755987c59cbbbf19a5e60524ab5e5f7933dc4b8
Eva <mac@macs-MacBook-Pro.local>
2021-12-14 15:49:01 +0100

    first commit

---
+++ component/Layout.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Head from "next/head";
+import Sidebar from "./Sidebar";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Course manager</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Head>
+      <div className="bg-gray-200 min-h-screen">
+        <div className="flex min-h-screen">
+          <Sidebar />
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
---
+++ component/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+   return (
+      <aside className='bg-gray-800 sm:w-1 xl:w-1/4 p-5 sm:min-h-screen'>
+         <p className='text-white text-2xl font-black'>Course Manager</p>
+      </aside>
+   )
+}
+
+export default Sidebar
