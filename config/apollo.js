a9f4947722bf39d5e991d8947701c31c2ab82e14
Eva <mac@macs-MacBook-Pro.local>
2021-12-20 15:49:32 +0100

    variables on vercel not working

--- config/apollo.js
+++ config/apollo.js
@@ -2,13 +2,13 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import fetch from "node-fetch";
 
 const client = new ApolloClient({
-   connectToDevTools:true,
-   cache : new InMemoryCache(),
-   link: new HttpLink({
-      uri: process.env.PORT_URL,
-      fetch
-   })
-
+  connectToDevTools: true,
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri:
+      "https://coursesmanagerserver.herokuapp.com/" || "http://localhost:4000/",
+    fetch,
+  }),
 });
 
 export default client;
